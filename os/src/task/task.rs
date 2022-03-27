790738ca447d0b1963a91e02c12ba2aa05dc185c
itewqq <qushipei1998@outlook.com>
2022-03-27 23:49:12 +0800

    Update TaskControlBlock

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,8 +1,19 @@
 use crate::mm::{MapPermission, MemorySet, PhysPageNum, VirtAddr, KERNEL_SPACE};
 use crate::config::{kernel_stack_position, TRAP_CONTEXT};
+use crate::sync::UPSafeCell;
 use crate::trap::{TrapContext, trap_handler};
 
 use super::TaskContext;
+use super::pid::{PidHandle, KernelStack};
+
+use core::cell::RefMut;
+use alloc::{
+    sync::{
+        Weak,
+        Arc,
+    },
+    vec::Vec,
+};
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum TaskStatus {
@@ -13,17 +24,27 @@ pub enum TaskStatus {
 }
 
 pub struct TaskControlBlock {
-    pub id: usize,
-    pub task_status: TaskStatus,
-    pub task_cx: TaskContext,
-    pub memory_set: MemorySet,
+    // immutable
+    pub pid: PidHandle,
+    pub kernel_stack: KernelStack,
+    // mutable
+    inner: UPSafeCell<TaskControlBlockInner>,
+}
+
+pub struct TaskControlBlockInner {
     pub trap_cx_ppn: PhysPageNum,
     pub base_size: usize,
     pub priority: usize,
     pub pass: usize,
+    pub task_cx: TaskContext,
+    pub task_status: TaskStatus,
+    pub memory_set: MemorySet,
+    pub parent: Option<Weak<TaskControlBlock> >,
+    pub children: Vec<Arc<TaskControlBlock> >,
+    pub exit_code: i32,
 }
 
-impl TaskControlBlock {
+impl TaskControlBlockInner {
     pub fn get_trap_cx(&self) -> &'static mut TrapContext {
         self.trap_cx_ppn.get_mut()
     }
@@ -32,39 +53,61 @@ impl TaskControlBlock {
         self.memory_set.token()
     }
 
+    pub fn get_status(&self) -> TaskStatus {
+        self.task_status
+    }
+}
+
+impl TaskControlBlock {
+    pub fn inner_exclusive_access(&self) -> RefMut<'_, TaskControlBlockInner> {
+        self.inner.exclusive_access()
+    }
+    
+    pub fn getpid(&self) -> usize {
+        self.pid.0
+    }
+
     pub fn new(elf_data: &[u8], app_id: usize) -> Self {
-        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
-        let trap_cx_ppn = memory_set
-            .translate(VirtAddr::from(TRAP_CONTEXT).into())
-            .unwrap()
-            .ppn();
-        let task_status = TaskStatus::Ready;
-        // map a kernel-stack in kernel space
-        let (kernel_stack_bottom, kernel_stack_top) = kernel_stack_position(app_id);
-        KERNEL_SPACE.exclusive_access().insert_framed_area(
-            kernel_stack_bottom.into(),
-            kernel_stack_top.into(),
-            MapPermission::R | MapPermission::W,
-        ).unwrap();
-        let task_control_block = Self {
-            id: app_id,
-            task_status,
-            task_cx: TaskContext::goto_trap_return(kernel_stack_top),
-            memory_set,
-            trap_cx_ppn,
-            base_size: user_sp,
-            priority: 16,
-            pass: 0,
-        };
-        // prepare TrapContext in user space
-        let trap_cx = task_control_block.get_trap_cx();
-        *trap_cx = TrapContext::app_init_context(
-            entry_point,
-            user_sp,
-            KERNEL_SPACE.exclusive_access().token(),
-            kernel_stack_top,
-            trap_handler as usize,
-        );
-        task_control_block
+        unimplemented!()
+        // let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
+        // let trap_cx_ppn = memory_set
+        //     .translate(VirtAddr::from(TRAP_CONTEXT).into())
+        //     .unwrap()
+        //     .ppn();
+        // let task_status = TaskStatus::Ready;
+        // // map a kernel-stack in kernel space
+        // let (kernel_stack_bottom, kernel_stack_top) = kernel_stack_position(app_id);
+        // KERNEL_SPACE.exclusive_access().insert_framed_area(
+        //     kernel_stack_bottom.into(),
+        //     kernel_stack_top.into(),
+        //     MapPermission::R | MapPermission::W,
+        // ).unwrap();
+        // let task_control_block = Self {
+        //     id: app_id,
+        //     task_status,
+        //     task_cx: TaskContext::goto_trap_return(kernel_stack_top),
+        //     memory_set,
+        //     trap_cx_ppn,
+        //     base_size: user_sp,
+        //     priority: 16,
+        //     pass: 0,
+        // };
+        // // prepare TrapContext in user space
+        // let trap_cx = task_control_block.get_trap_cx();
+        // *trap_cx = TrapContext::app_init_context(
+        //     entry_point,
+        //     user_sp,
+        //     KERNEL_SPACE.exclusive_access().token(),
+        //     kernel_stack_top,
+        //     trap_handler as usize,
+        // );
+        // task_control_block
+    }
+
+    pub fn exec(&self, elf_data: &[u8]) {
+        unimplemented!()
+    }
+    pub fn fork(self: &Arc<TaskControlBlock>) -> Arc<TaskControlBlock> {
+        unimplemented!()
     }
 }
