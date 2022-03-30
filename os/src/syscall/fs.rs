d4108baae8f2231cc76eaa4162c6af75519b0757
itewqq <qushipei1998@outlook.com>
2022-03-31 00:32:37 +0800

    Add sys_read

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,6 +1,10 @@
-use crate::mm::translated_byte_buffers;
-use crate::task::current_user_token;
+use core::panic;
 
+use crate::sbi::console_getchar;
+use crate::mm::translated_byte_buffers;
+use crate::task::{current_user_token, suspend_current_and_run_next};
+
+const FD_STDIN: usize = 0;
 const FD_STDOUT: usize = 1;
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
@@ -14,6 +18,35 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
         }
         _ => {
             panic!("Unsupported fd in sys_write!");
+        }
+    }
+}
+
+pub fn sys_read(fd: usize, buf: *const u8, len: usize) -> isize {
+    match fd {
+        FD_STDIN => {
+            assert_eq!(len, 1);
+            let mut c: usize;
+            loop {
+                c = console_getchar();
+                if c == 0 {
+                    suspend_current_and_run_next();
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            let ch = c as u8;
+            let mut buffers = translated_byte_buffers(
+                current_user_token(),
+                buf,
+                len,
+            );
+            unsafe { buffers[0].as_mut_ptr().write_volatile(ch); }
+            1
+        },
+        _ => {
+            panic!("Unsupported fd in sys_read!");
         }
     }
 }
